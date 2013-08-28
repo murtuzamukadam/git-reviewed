32c4a7b6314e99d9a79e2e667b3efc99c8b05b78
Murtuza Mukadam <murtuza16289@gmail.com>
2013-08-28 11:11:56 -0400

    better indentation of scripts for understanding

--- scripts/make reviews /makeReview.pl
+++ scripts/make reviews /makeReview.pl
@@ -8,16 +8,15 @@ my $dbName = 'data.db';
 my $email = ' ';
 my @tid=0;
 my $dbh = DBI->connect("dbi:SQLite:dbname=$dbName", '', '') or die "Cannot connect: $DBI::errstr";
-
- my $parseropts = {
+my $parseropts = {
         enable_cache    => 1,
         enable_grep     => 1,
         cache_file_name => 'mail/cache-file',
-    };
-    my $mb = Mail::MboxParser->new($ARGV[0],
+     };
+my $mb = Mail::MboxParser->new($ARGV[0],
                                     decode     => 'ALL',
                                     parseropts => $parseropts);
-    for my $msg ($mb->get_messages) {
+for my $msg ($mb->get_messages) {
        
        my $referenceid = $msg->header->{references}, "\n";
        my $from = $msg->header->{from}, "\n";
@@ -29,18 +28,13 @@ my $dbh = DBI->connect("dbi:SQLite:dbname=$dbName", '', '') or die "Cannot conne
        my $thisid = $msg->id;
        my $body = $msg->body;
        my $body_str = $body->as_string || '<No message text>';
-
-
-    my $sth = $dbh->prepare("SELECT * FROM line where tid='$thisid'");
-    $sth->execute;
+       my $sth = $dbh->prepare("SELECT * FROM line where tid='$thisid'");
+       $sth->execute;
+       my $row = $sth->fetchrow_arrayref();
     
-    my $row = $sth->fetchrow_arrayref();
-    
-  if ($row) {
+       if ($row) {
               my ($tid, $commitid) = @$row;
-              
               open (MYFILE, '> data.tmp');
-              print MYFILE "commit: $commitid\n\n";
               print MYFILE "From : $from\n";
               if ( $from =~ /<(.*?)>/ )
 		{
@@ -58,65 +52,51 @@ my $dbh = DBI->connect("dbi:SQLite:dbname=$dbName", '', '') or die "Cannot conne
               if(length($replyto ne 0)) {
                   print MYFILE "$replyto\n";
                   }        
-              
               print MYFILE "Cc: $cc\n";
-             
+              print MYFILE "commit: $commitid\n\n";
               print MYFILE "$body_str";
               close (MYFILE); 
-              print "This is a thread \n";
               my $show =`git reviewmbox '$commitid' '$from' '$date' '$email'`;
               print $show;
               
              
 
-            } 
+          } 
 
 
-elsif ( $referenceid =~ /<(.*?)>/ ){
-           my $reference = $1;   
-
- my $sth = $dbh->prepare("SELECT * FROM line where tid='$reference'");
-    $sth->execute;
-    my $row = $sth->fetchrow_arrayref();
-    
-    
-
-        
-           if ($row) {
-              my ($tid, $commitid) = @$row;
-              open (MYFILE, '> data.tmp');
-              print MYFILE "commit: $commitid\n\n";
-              print MYFILE "From : $from\n";
-              if ( $from =~ /<(.*?)>/ )
-		{
+      elsif ( $referenceid =~ /<(.*?)>/ ){
+             my $reference = $1;   
+             my $sth = $dbh->prepare("SELECT * FROM line where tid='$reference'");
+             $sth->execute;
+             my $row = $sth->fetchrow_arrayref();       
+             if ($row) {
+                my ($tid, $commitid) = @$row;
+                open (MYFILE, '> data.tmp');
+                print MYFILE "From : $from\n";
+                if ( $from =~ /<(.*?)>/ )
+		 {
    		   $email = $1;
     		  
-		}
-              print MYFILE "Message-Id : <$thisid>\n";
-              print MYFILE "Date : $date\n";
+		 }
+               print MYFILE "Message-Id : <$thisid>\n";
+               print MYFILE "Date : $date\n";
                print MYFILE "Subject: $subject\n";
-              print MYFILE "$replyto\n";
-              print MYFILE "References : $referenceid\n";
-             
-              print MYFILE "Cc: $cc\n";
-              
-              print MYFILE "$body_str";
-              close (MYFILE);   
-              print "This is a response \n";  
-              my $response =`git reviewmbox '$commitid' '$from' '$date' '$email'`;
-              print $response;
+               print MYFILE "$replyto\n";
+               print MYFILE "References : $referenceid\n";
+               print MYFILE "Cc: $cc\n";
+               print MYFILE "commit: $commitid\n\n";
+               print MYFILE "$body_str";
+               close (MYFILE);   
+               my $response =`git reviewmbox '$commitid' '$from' '$date' '$email'`;
+               print $response;
             
-          }
+              }
          
        }
 
 
         
 }
-
-
-
-
 
 $dbh->disconnect();
 exit 0;
